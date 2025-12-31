1b8fc6a6a48e9e61ce38d4fc29f1cf6a2a4456df
RITHESH <rithesh.konda23@gmail.com>
2025-12-31 10:26:58 +0530

    commited

---
+++ PrimeNumberProj/src/main/java/com/su2/APP1.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import java.util.Scanner;
+
+class PrimeChecker {
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (isPrime(n)) {
+            System.out.println(n + " is a Prime number");
+        } else {
+            System.out.println(n + " is NOT a Prime number");
+        }
+        sc.close();
+    }
+}
