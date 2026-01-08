ae2194053d27483491785ad71f4dd67cedb15c28
RITHESH <rithesh.konda23@gmail.com>
2026-01-08 13:59:30 +0530

    maven project

---
+++ maven_demo/EmployeeMavenJdbc_SU2/src/main/java/com/example/DBUtil.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/company_db",
+                "root",
+                "");
+    }
+}
